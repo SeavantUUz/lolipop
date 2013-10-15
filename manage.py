3d6ace9f4cc84b9257162bb1e7e5ef39752be49e
AprocySanae <axdiaoqi220@gmail.com>
2013-10-15 21:38:59 +0000

    now which could add new post

--- manage.py
+++ manage.py
@@ -2,9 +2,9 @@ import os
 from flask.ext.script import Manager,Server
 from config import db
 from app import create_app
-from kutoto.form import NewTopic
+from kutoto.form import NewTopic,ReplyPost
 from kutoto.models import Post,Topic
-from flask import redirect,render_template
+from flask import redirect,render_template,url_for
 
 app = create_app()
 manager = Manager(app)
@@ -12,6 +12,7 @@ manager.add_command("runserver",Server("localhost",port=8000))
 
 
 @app.route("/")
+@app.route("/index")
 def index():
     topics = Topic.query.all()
     return render_template('index.html',topics=topics)
@@ -24,10 +25,16 @@ def new_topic():
         return redirect('/index')
     return render_template('form.html',form=form)
 
-@app.route("/topic/<int:topic_id>")
+@app.route("/topic/<int:topic_id>",methods=('GET','POST'))
 def show_topic(topic_id):
+    form = ReplyPost()
     posts = Post.query.filter_by(topic_id=topic_id).order_by(Post.date_created)
-    return render_template('posts.html',posts=posts)
+    topic = Topic.query.get(topic_id)
+    if form.validate_on_submit():
+        form.save(topic)
+        return redirect(url_for('show_topic',topic_id=topic_id))
+        #return redirect('/index')
+    return render_template('posts.html',posts=posts,form=form)
 
 @manager.command
 def init():
