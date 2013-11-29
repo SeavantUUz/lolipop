4b6fc756496398536f07b3f46208e9b28a9d546c
AprocySanae <axdiaoqi220@gmail.com>
2013-11-28 22:54:56 -0600

    add context_processor

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from config import db,BleepRenderer
+import os,codecs
 import misaka as m
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = '\x03\xedS\x08d`\xb0\x97_\x960x\xac\x12\x87\x88\x9f@x:n`\xeb\xd5'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['ROOTDIR'] = os.getcwd()
     app.debug = True
     register_jinja(app)
     db.init_app(app)
+    if not os.path.exists('announcement'):
+        os.mknod('announcement')
     return app
 
 def register_jinja(app):
+    from views.admin import load_sidebar_notice
 
     @app.template_filter('renderToGFM')
     def renderToGFM(data):
@@ -19,3 +24,10 @@ def register_jinja(app):
         md = m.Markdown(renderer,
                 extensions=m.EXT_FENCED_CODE | m.EXT_NO_INTRA_EMPHASIS)
         return md.render(data)
+
+    @app.context_processor
+    def register_context():
+        return dict(
+                load_sidebar_notice=load_sidebar_notice,
+                )
+
--- views/admin.py
+++ views/admin.py
@@ -1,15 +1,19 @@
 # coding:utf-8
-from flask import Blueprint,request,render_template,flash
+from flask import Blueprint,request,render_template,flash,current_app,redirect,url_for
 from views.account import admin_required
 from config import force_int,fill_object
 from kutoto.models import User,Profile
 from kutoto.form import NodeForm
+import codecs,os
 
 bp = Blueprint('admin',__name__)
 
 @bp.route('/',methods=['GET','POST'])
 @admin_required
 def dashboard():
+    if request.method == 'POST':
+        save_sidebar_notice(request.form.get('content',None))
+        return redirect(url_for('.dashboard'))
     page = force_int(request.args.get('page', 1), 0)
     if not page:
         return abort(404)
@@ -17,8 +21,19 @@ def dashboard():
     paginator = User.query.order_by(User.id.desc()).paginate(page)
     profiles = Profile.query.order_by(Profile.id).paginate(page).items
     paginator.itmes = fill_object(paginator.items,profiles,'avatar')
+
     return render_template(
         'admin/dashboard.html',
         paginator=paginator,
         form = form,
     )
+
+def save_sidebar_notice(content):
+    with codecs.open(os.path.join(current_app.config['ROOTDIR'],'announcement'),'w','utf-8','strict') as f:
+        f.write(content)
+
+def load_sidebar_notice():
+    with codecs.open(os.path.join(current_app.config['ROOTDIR'],'announcement'),'r','utf-8','strict') as f: 
+        content = f.read()
+        return content
+
